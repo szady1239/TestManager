a1e99e2174d872109fd30833f8a4e89fe6e18b79
PKOWALCZYK <pawel.kowalczyk@execon.pl>
2018-02-20 08:29:40 +0100

    Import project

---
+++ src/main/java/pl/szady/testmanager/TestmanagerApplication.java
@@ -0,0 +1,12 @@
+package pl.szady.testmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmanagerApplication.class, args);
+	}
+}
